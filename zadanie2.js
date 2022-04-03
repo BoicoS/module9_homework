ef3daa88f16eecb532ad9b0263fa3952f83d5681
Uliana <boicos06@gmail.com>
2022-04-03 16:09:53 +0300

    mod9-2

---
+++ zadanie2.js
@@ -0,0 +1,17 @@
+const str = `{
+    "list": [
+     {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+     },
+     {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+     }
+    ]
+   }`;
+
+const parser =JSON.parse(str);
+console.log(parser);
